5b53a1b7dc365e5bcb3279aa816c7ffccf739fe0
Marius ballot <marius@haply.co>
2022-12-09 14:28:38 +0100

    first commi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import Timeline from "./components/timeline/timeline.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <Timeline />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/timeline/timeline.jsx
@@ -0,0 +1,11 @@
+import "./timeline.scss"
+
+function Timeline() {
+  return (
+    <div className="timeline">
+      <h1>Timeline component</h1>
+    </div>
+  );
+}
+
+export default Timeline;
